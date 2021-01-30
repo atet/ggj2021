a862acd1fa04de26bdcdb48b460d93d881346c5e
atet <atet.kao@uci.edu>
2021-01-30 09:36:24 -0800

    Audio test prototype complete.

--- test_audio/Assets/Scripts/Movement.cs
+++ test_audio/Assets/Scripts/Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public AudioSource footsteps;
+    public AudioSource yell;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +17,34 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("Up"))
+        if (Input.GetKey("right"))
         {
-            transform.position += new Vector3(0, 0, 1 * Time.deltaTime);
+            transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("Down"))
+        if (Input.GetKey("left"))
         {
-            transform.position += new Vector3(0, 0, -1 * Time.deltaTime);
+            transform.position += new Vector3(-1 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown("right") | Input.GetKeyDown("left"))
+        {
+            if (!footsteps.isPlaying)
+            {
+                footsteps.Play();
+            }
+        }
+        if (Input.GetKeyUp("right") | Input.GetKeyUp("left"))
+        {
+            if (footsteps.isPlaying)
+            {
+                footsteps.Stop();
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if (!yell.isPlaying)
+            {
+                yell.Play();
+            }
         }
     }
 }
